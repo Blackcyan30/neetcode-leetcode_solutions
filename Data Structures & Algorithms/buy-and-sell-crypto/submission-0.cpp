bddaf35de0345af2fea91a05c9102470c28fb63e
Talha Adnan <mkhan387@uic.edu>
2026-09-07 00:40:53 -0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l{0};
+        int max_price{0};
+        for (size_t r{0}; r < prices.size(); ++r) {
+            if (prices[l] < prices[r]) {
+                max_price = std::max(max_price, prices[r] - prices[l]);
+            } else {
+                l = r;
+            }
+        }
+        return max_price;
+    }
+};
